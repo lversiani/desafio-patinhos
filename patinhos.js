d6c227c41c07c0786e96dc87ff1c4df3a79eb648
Letícia Versiani <64595680+lversiani@users.noreply.github.com>
2022-07-26 15:12:21 -0300

    Add files via upload

---
+++ patinhos.js
@@ -0,0 +1,9 @@
+let n = parseInt(gets());      
+while(n != -1) { 
+        if (n > 0){
+            print(n - 1);
+        }
+        else 
+            print(n);
+        n = parseInt(gets()); 
+    }
